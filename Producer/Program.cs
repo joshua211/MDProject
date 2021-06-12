3953f4e0eb8b72b40ebea3996ad518656a602376
fhdoVM <fhdoVM@fh-dortmund.de>
2021-06-12 12:09:37 +0200

    move to csharp

---
+++ Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using System.Text.Json;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "172.22.160.151:9092",
+                ClientId = Dns.GetHostName(),
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var test = new TestClass()
+                {
+                    Id = 1,
+                    Name = "Test haha"
+                };
+                var json = JsonSerializer.Serialize(test, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                var result = await producer.ProduceAsync("myTestTopic", new Message<Null, string>() { Value = json }, CancellationToken.None);
+                System.Console.WriteLine(result.Status);
+            }
+        }
+    }
+}
---
+++ Producer/TestClass.cs
@@ -0,0 +1,9 @@
+namespace Producer
+{
+    class TestClass
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+    }
+}
