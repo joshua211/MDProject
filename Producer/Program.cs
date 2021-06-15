989ecc8ff6f10775b2b97576d14b39fcb1425244
joshua211 <lycosh211@gmx.de>
2021-06-15 11:16:26 +0200

    add timeout and order size/symbol
    Co-authored-by: lucasschoenhold <lucasschoenhold@users.noreply.github.com>

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Timers;
+using System;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,7 +11,6 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using Serilog;
 using Serilog.Events;
-using Serilog.Filters;
 
 namespace Producer
 {
@@ -33,13 +33,23 @@ namespace Producer
             config = new AppConfig("appsettings.json");
             string server = config.Get<string>("server");
             var products = config.Get<List<string>>("products");
+            var timeout = int.Parse(config.Get<string>("timeout"));
+
+            Log.Debug("Waiting for {Timeout}ms", timeout);
+            await Task.Delay(timeout);
 
             Log.Information("Starting connector for server {Server}", server);
 
             var coinbaseClient = new CoinbaseProClient();
 
             var socket = coinbaseClient.WebSocket;
-            socket.OnMatchReceived += (sender, args) => queue.Enqueue(new Trade(args.LastOrder.TradeId, args.LastOrder.Time.Date, args.LastOrder.Price));
+            socket.OnMatchReceived += (sender, args) => queue.Enqueue(new Trade(
+                args.LastOrder.TradeId,
+                args.LastOrder.Time.Date,
+                args.LastOrder.Price,
+                args.LastOrder.Size,
+                args.LastOrder.ProductId));
+            socket.OnErrorReceived += (sender, args) => Log.Warning("Error: {Message}", args.LastOrder.Message);
 
             Log.Debug("Starting websocket for products: {@Products}", products);
             socket.Start(products, new List<ChannelType>() { ChannelType.Matches });
@@ -55,29 +65,28 @@ namespace Producer
         private static async Task Run(ProducerConfig producerConfig, CancellationToken token)
         {
             Log.Debug("Building producer with config {@Config}", producerConfig);
-            using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
-            {
-                var topic = config.Get<string>("topic");
-                while (!token.IsCancellationRequested)
-                {
-                    if (!queue.TryDequeue(out var trade))
-                        continue;
+            using var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
 
-                    var json = JsonSerializer.Serialize(trade, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-                    Log.Debug("Writing trade {Trade} to topic {Topic}", json, topic);
-                    try
-                    {
-                        var resultToken = new CancellationTokenSource(10000);
-                        var result = await producer.ProduceAsync(topic, new Message<Null, string>() { Value = json }, resultToken.Token);
-                        if (result.Status == PersistenceStatus.Persisted)
-                            Log.Debug("Successfully persisted");
-                        else
-                            Log.Warning("Failed to write to topic {topic}: {Result}", topic, result.Value);
-                    }
-                    catch (Exception e)
-                    {
-                        Log.Error(e, "Failed to produce message");
-                    }
+            var topic = config.Get<string>("topic");
+            while (!token.IsCancellationRequested)
+            {
+                if (!queue.TryDequeue(out var trade))
+                    continue;
+
+                var json = JsonSerializer.Serialize(trade, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                Log.Debug("Writing trade {Trade} to topic {Topic}", json, topic);
+                try
+                {
+                    var resultToken = new CancellationTokenSource(10000);
+                    var result = await producer.ProduceAsync(topic, new Message<Null, string>() { Value = json }, resultToken.Token);
+                    if (result.Status == PersistenceStatus.Persisted)
+                        Log.Debug("Successfully persisted");
+                    else
+                        Log.Warning("Failed to write to topic {topic}: {Result}", topic, result.Value);
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "Failed to produce message");
                 }
             }
         }
--- Producer/Trade.cs
+++ Producer/Trade.cs
@@ -5,14 +5,20 @@ namespace Producer
     public class Trade
     {
         public long TradeId { get; set; }
+        public string Symbol { get; set; }
         public DateTime Time { get; set; }
         public decimal Price { get; set; }
+        public decimal Size { get; set; }
 
-        public Trade(long tradeId, DateTime time, decimal price)
+
+
+        public Trade(long tradeId, DateTime time, decimal price, decimal size, string symbol)
         {
             TradeId = tradeId;
             Time = time;
             Price = price;
+            Size = size;
+            Symbol = symbol;
         }
     }
 }
